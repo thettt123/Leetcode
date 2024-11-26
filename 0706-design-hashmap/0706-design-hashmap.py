1473e76566a963ab358c53d6e82800cd1fbeaa10
thettt123 <79957399+thettt123@users.noreply.github.com>
2024-11-26 17:17:34 +0900

    Time: 38 ms (65.60%), Space: 42.8 MB (5.89%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.py
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -1,22 +1,22 @@
 class MyHashMap:
 
     def __init__(self):
-        m=10**6+5
-        self.lookup=[None]*m
+        m = 10**6+5
+        self.lookup = [None]*m
         
 
     def put(self, key: int, value: int) -> None:
-        self.lookup[key]=value
+        self.lookup[key] = value
 
     def get(self, key: int) -> int:
-        if self.lookup[key]==None:
+        if self.lookup[key] == None:
             return -1
         
         return self.lookup[key]
 
 
     def remove(self, key: int) -> None:
-        self.lookup[key]=None
+        self.lookup[key] = None
         
 
 
