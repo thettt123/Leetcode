2ec4649e02ff91b0c945d32ceafe8f61cd3933ef
thettt123 <79957399+thettt123@users.noreply.github.com>
2024-11-27 14:07:14 +0900

    Time: 16 ms (52.67%), Space: 20.5 MB (39.14%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findJudge(self, n: int, trust: list[list[int]]) -> int:
+        degrees = list(chain((-1,), repeat(0, n), (n - 1,)))
+        
+        for u, v in trust: degrees[u] -= 1; degrees[v] += 1
+        
+        judge = degrees.index(n - 1)
+        
+        return judge if judge <= n else -1
